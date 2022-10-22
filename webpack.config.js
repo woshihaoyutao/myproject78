6a4d11eb08e7f1d65d090da666a84f0b691c1313
hyt <1151123047@qq.com>
2022-10-22 16:39:31 +0800

    已完成webpack单入口多文件练习

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
-// const path = require("path");
-// module.exports = {
-//   entry: "./src/index.js",
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//     filename: "main.js",
-//   },
-//   mode: "development",
-// };
+const path = require("path");
+console.log("webpack.config.js模块被执行了");
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+    one: "./src/one.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].main.js",
+  },
+  mode: "development",
+};
