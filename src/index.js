fa2039febec9ef4a60fff7a652d21f50dbaac151
hyt <1151123047@qq.com>
2022-10-22 15:31:39 +0800

    webpack单文件单入口练习

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import Str from "./str";
+const app = document.getElementById("app");
+const h3 = "<h3>首页哈哈哈</h3>";
+app.innerHTML = h3 + Str.link;
---
+++ src/str.js
@@ -0,0 +1,3 @@
+export default {
+  link: '<a href="http://www.tmall.com">天猫 </a>',
+};
